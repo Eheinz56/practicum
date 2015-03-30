1905332a80077026f0d3418046d2218e8c2206bd
Eric Heinzman <ehu9079@ehu9079-VirtualBox.(none)>
2015-03-30 12:47:55 -0400

    modified counter.js to work properly for exam question

--- counter.js
+++ counter.js
@@ -5,6 +5,10 @@ function minusOne(match, amount, unit) {
     unit = unit.slice(0, unit.length - 1);
   else if (amount == 0)
     amount = "no";
-  return amount + " " + unit;
+  else if (amount == 100)
+ //if 100 is met then then line below will print
+    return 'zillions of eggs';  
+  return amount + " " + unit ;
+
 }
 print(stock.replace(/(\d+) (\w+)/g, minusOne));
